f81c55b5d1bee5c53f6c24185c751d765aa03064
Shreya Goti <66583470+shreyagoti09@users.noreply.github.com>
2021-03-31 16:49:27 +0530

    some changes

--- Food-Mania/admin/delete1.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-$id=$_GET["id"];
-mysqli_query($conn,"delete from tables where id='$id'");
-?>
-<script type="text/javascript">
-	alert("Deleted");
-	window.location="tables.php"; 	
-</script>
